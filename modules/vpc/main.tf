b49d2687353cd66234ba3a486cea84c1968e4a11
Claudio Bianco <91281998+claudio-bianco@users.noreply.github.com>
2025-10-16 14:21:53 -0300

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -3,8 +3,7 @@ resource "aws_vpc" "this" {
   enable_dns_support   = true
   enable_dns_hostnames = true
 
-  tags = merge(
-    var.default_tags,
-    { Name = "vpc-${var.env}" }
-  )
+  tags = {
+    Name = "vpc-${var.env}"
+  }
 }
